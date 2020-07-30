09a361ee47c9c00f221b829810a4299dcb08c195
= <=>
2020-07-31 03:58:10 +0530

    [Vishnu]Refactor.added add books and get books with user id

--- model/admin.js
+++ model/admin.js
@@ -1,7 +1,12 @@
 const mongoose = require("mongoose");
 var mongoSchema = mongoose.Schema;
+const Schema = mongoose.Schema;
 var adminSchema = new mongoSchema(
   {
+    user_id: {
+      type: String,
+      required: [true, "User_id required"],
+    },
     bookName: { type: String, required: [true, "bookname is required"] },
     authorName: { type: String, required: [true, "authorname is required"] },
     title: { type: String, required: [true, "title is required"] },
@@ -36,13 +41,24 @@ bookModel.prototype.addBooks = (req) => {
 };
 bookModel.prototype.getBooks = (field, callback) => {
   try {
-    Books.find(field, (err, result) => {
-      if (err) {
-        return callback(err);
-      } else {
-        return callback(null, result);
-      }
+    return new Promise((resolve, reject) => {
+      Books.find({ user_id: field })
+        .then((result) => {
+          resolve({ data: result });
+        })
+        .catch((err) => {
+          reject({ error: err });
+        });
     });
+    // console.log(field);
+
+    // Books.find({ user_id: field }, (err, result) => {
+    //   if (err) {
+    //     return callback(err);
+    //   } else {
+    //     return callback(null, result);
+    //   }
+    // });
   } catch (err) {
     return callback(err);
   }
