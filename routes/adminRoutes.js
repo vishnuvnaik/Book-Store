9d13e48107b619187c5248284b219c22fa5c7f5e
= <=>
2020-07-31 03:58:37 +0530

    [Vishnu]Refactor.added route for get books

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const adminController = require("../controller/adminController");
 const router = express.Router();
+const authenticate = require("../middleware/autheticate");
+
 // user routes
-router.get("/books", adminController.getBooks);
-router.post("/books", adminController.addBookController);
+router.get("/books", authenticate.checkTokenAuth, adminController.getBooks);
+router.post("/books",authenticate.checkTokenAuth, adminController.addBookController);
 //router.put("/books", adminController.updateBooks);
 //router.delete("/books/", token.verify, adminController.deleteBooks);
 module.exports = router;
