9781b45953db18755723cfd659b5e77b1da73f56
= <=>
2020-07-31 18:54:49 +0530

    [Vishnu]Refactor.added proper routes for update book

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -5,7 +5,15 @@ const authenticate = require("../middleware/autheticate");
 
 // user routes
 router.get("/books", authenticate.checkTokenAuth, adminController.getBooks);
-router.post("/books",authenticate.checkTokenAuth, adminController.addBookController);
-//router.put("/books", adminController.updateBooks);
-//router.delete("/books/", token.verify, adminController.deleteBooks);
+router.post(
+  "/books",
+  authenticate.checkTokenAuth,
+  adminController.addBookController
+);
+router.put("/books/:_id", adminController.updateBooks);
+// router.delete(
+//   "/books/:_id",
+//   authenticate.checkTokenAuth,
+//   adminController.deleteBooks
+// );
 module.exports = router;
