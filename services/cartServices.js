5b0aff9a1d59cfc764befc7c5e2118d6d211e0c7
vishnu vnaik <vishnuvdev17@gmail.com>
2020-08-03 13:09:36 +0530

    [Vishnu]Refactor.modified add to cart by comparing the input and quantity from data

--- services/cartServices.js
+++ services/cartServices.js
@@ -3,38 +3,62 @@ const bookModel = require("../model/admin");
 
 exports.addToCart = (req, callback) => {
   try {
-
-    bookModel.getAvailableBooks({ bookId: req.book_id },
-      (err, data) => {
-        console.log(req)
-        console.log(data.quantity)
-        let quan = req.quantity;
-        let quandata = data.quantity;
-        if (err) {
-          return callback("error occured");
-        }
-        else if (quan < quandata) {
-          cartModel.addToCart(
-            req,
-            (err, data) => {
-              if (err) {
-                console.log("error")
-                return callback(err, null);
-              } else {
-                console.log("data is")
-                return callback(null, data);
-
-              }
-            }
-          );
+    return new Promise((resolve, reject) => {
+      bookModel.getAvailableBooks({ bookId: req.book_id }).then((data) => {
+        console.log(data.data.quantity)
+        if (data.data.quantity > req.quantity) {
+          resolve(data)
+          return new Promise((resolve, reject) => {
+            cartModel
+              .addToCart(req)
+              .then((data) => {
+                resolve(data);
+              })
+              .catch((err) => {
+                reject(err);
+              })
+          })
         }
         else {
-          return callback("quantity is less ");
+          return callback(err)
         }
-      });
+      }).catch((err) => {
+        reject(err);
+      })
+    })
+
   } catch (err) {
     return err;
   }
+
+  //bookModel.getAvailableBooks({ bookId: req.book_id },
+  // (err, data) => {
+  //   console.log(req)
+  //   console.log(data.quantity)
+  //   let quan = req.quantity;
+  //   let quandata = data.quantity;
+  //   if (err) {
+  //     return callback("error occured");
+  //   }
+  //   else if (quan < quandata) {
+  //     cartModel.addToCart(
+  //       req,
+  //       (err, data) => {
+  //         if (err) {
+  //           console.log("error")
+  //           return callback(err, null);
+  //         } else {
+  //           console.log("data is")
+  //           return callback(null, data);
+  //         }
+  //       }
+  //     );
+  //   }
+  //   else {
+  //     return callback("quantity is less ");
+  //   }
+  // });
+
 };
 exports.getAllItemsFromCart = (req, callBack) => {
 
