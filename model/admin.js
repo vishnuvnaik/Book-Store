061eb5a7743b151a78eefe00f3d5631744f0ecb5
vishnu vnaik <vishnuvdev17@gmail.com>
2020-08-05 17:47:00 +0530

    [Vishnu]Added.genre in schema

--- model/admin.js
+++ model/admin.js
@@ -4,7 +4,7 @@ var adminSchema = new mongoSchema(
   {
     bookName: { type: String, required: [true, "bookname is required"] },
     authorName: { type: String, required: [true, "authorname is required"] },
-    title: { type: String, required: [true, "title is required"] },
+    genre: { type: String, required: [true, "genre is required"] },
     description: { type: String, required: [true, "description is required"] },
     quantity: { type: Number, required: [true, "quantity is required"] },
     price: { type: Number, required: [true, "price is required"] },
@@ -18,28 +18,15 @@ var Books = mongoose.model("Books", adminSchema);
 function bookModel() { }
 
 bookModel.prototype.addBooks = (req) => {
-  try {
-    return new Promise((resolve, reject) => {
-      let bookAdd = new Books(req);
-      bookAdd
-        .save()
-        .then((result) => {
-          resolve({ data: result });
-        })
-        .catch((err) => {
-          reject({ error: err });
-        });
-    });
-  } catch (err) {
-    console.log(err);
-  }
-};
+  let bookAdd = new Books(req);
+  return bookAdd.save()
+}
 bookModel.prototype.getBooks = (field) => {
   return Books.find(field.find)
 }
 
 bookModel.prototype.getAvailableBooks = (field) => {
-  return Books.findById(field.bookId)
+  return Books.findById(field.product_id)
 };
 
 bookModel.prototype.updateBook = (_id, req) => {
