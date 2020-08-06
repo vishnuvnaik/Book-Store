49d58275d421c67d1b26264f3d55cdd6c52b7aa7
vishnu vnaik <vishnuvdev17@gmail.com>
2020-08-06 23:12:40 +0530

    [Vishnu]Refactor.modified order adding method,added userid checking from db

--- services/orderServices.js
+++ services/orderServices.js
@@ -1,16 +1,28 @@
 const orderModel = require("../model/order");
-
+const cartModel = require("../model/cart")
 module.exports.addOrder = (req) => {
     try {
         return new Promise((resolve, reject) => {
-            orderModel
-                .addOrder(req)
+            cartModel.getAllItemsFromCart(req.user_id)
                 .then((data) => {
-                    resolve(data);
+                    if (data.length <= 0) {
+                        reject("Cart is empty for this user id ")
+                    }
+                    else if (data.length > 0) {
+                        orderModel
+                            .addOrder(req)
+                            .then((data) => {
+                                resolve(data);
+                            })
+                            .catch((err) => {
+                                reject(err);
+                            });
+
+                    }
+                    else if (data == null) {
+                        reject("user id not found,try again")
+                    }
                 })
-                .catch((err) => {
-                    reject(err);
-                });
         });
     } catch (err) {
         return err;
