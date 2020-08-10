eb331195d66c805da8bc00d68a6f9643aef8bd02
vishnu vnaik <vishnuvdev17@gmail.com>
2020-08-11 03:41:20 +0530

    [Vishnu]Add.added redis cache,implementing search api

--- services/adminServices.js
+++ services/adminServices.js
@@ -1,10 +1,85 @@
 const bookModel = require("../model/admin");
+const redisDb = require('./cache')
+const logger = require("../config/logger")
+module.exports = class AdminServices {
+  async refreshRedis(uniqueId) {
+    try {
+      let redisResult = await redisDb.deleteKeys(uniqueId)
+      logger.info("redis result", redisResult)
+      if (redisResult) {
+        return redisResult
+      }
+      else {
+        return false
+      }
+    } catch (error) {
+      logger.error(error)
+      return error
+    }
+  }
+  addBooks = (req) => {
+    try {
+      let uniqueId = "books1234";
+      return new Promise((resolve, reject) => {
+        bookModel
+          .addBooks(req)
+          .then((data) => {
+            resolve(data);
 
-module.exports.addBooks = (req) => {
-  try {
+            if (data !== null) {
+              let refreshResult = this.refreshRedis(uniqueId)
+              if (refreshResult) {
+                logger.info(`Redis refreshed !`)
+                return { "success": true, "message": "Books added SUCCESFULLY !", "data": data }
+              } else { // problem occured while refreshing redis
+                logger.info(`Redis not refreshed .`)
+                return { "success": true, "message": "books added SUCCESFULLY (NO DATA IN REDIS TO REFRESH )", "data": noteResult }
+              }
+            }
+            else {
+              reject({ message: "data not received" })
+            }
+          })
+          .catch((err) => {
+            reject(err);
+          });
+      });
+    } catch (err) {
+      return err;
+    }
+  };
+  getAllBooksService = (req) => {
+    let findQuery = {
+      find: req.find,
+    };
+    try {
+      return new Promise((resolve, reject) => {
+        bookModel
+          .getBooks(findQuery)
+          .then((data) => {
+            resolve(data);
+
+            if (data.length > 0) {
+
+              let key = "books1234";
+              let redisResult = redisDb.setCache(key, JSON.stringify(data))
+              logger.info('redis result ', redisResult)
+              return { "success": true, "message": "ALL NOTES LOADED SUCCESSFULLY !", "data": notesResult }
+            }
+            else {
+              reject(err)
+            }
+          })
+          .catch((err) => reject(err));
+      });
+    } catch (err) {
+      return err
+    }
+  };
+  updateBooks = (_id, req) => {
     return new Promise((resolve, reject) => {
       bookModel
-        .addBooks(req)
+        .updateBook(_id, req)
         .then((data) => {
           resolve(data);
         })
@@ -12,48 +87,51 @@ module.exports.addBooks = (req) => {
           reject(err);
         });
     });
-  } catch (err) {
-    return err;
-  }
-};
-module.exports.getAllBooksService = (req) => {
-  let findQuery = {
-    find: req.find,
   };
-  try {
+  deleteBook = (_id) => {
     return new Promise((resolve, reject) => {
       bookModel
-        .getBooks(findQuery)
+        .deleteBook(_id)
         .then((data) => {
           resolve(data);
         })
-        .catch((err) => reject(err));
+        .catch((err) => {
+          reject(err);
+        });
     });
-  } catch (err) {
-    return err
+  };
+  async searchingBooks(searchingData) {
+    try {
+      let enteredData = searchingData.search
+      let findingQuery = {
+        $and: [{
+          $or:
+            [
+              { 'bookName': { $regex: enteredData, $options: 'i' } },
+              { 'description': { $regex: enteredData, $options: 'i' } },
+              { 'authorName': { $regex: enteredData, $options: 'i' } },
+              { 'price': { $regex: enteredData } },
+              { 'quantity': { $regex: enteredData } }
+            ]
+        }]
+      }
+      //firstly in the notes schema searching is done
+      let searchResultArray = await bookModel.searchingBooks(findingQuery)
+      logger.info("\n\n\tResult of searching --> ", searchResultArray);
+
+      if (searchResultArray.length > 0) {
+        return { "success": true, "message": "ALL books FOUND !", "data": searchResultArray }
+      } else {                    //no labels are found 
+        return { "success": false, "message": "NO books FOUND !", "data": searchResultArray }
+      }
+
+    } catch (error) {
+      logger.error(error);
+      let response = {}
+      response.success = false
+      response.message = `ERROR IN SERVICE`
+      response.error = error
+      return response
+    }
   }
-};
-module.exports.updateBooks = (_id, req) => {
-  return new Promise((resolve, reject) => {
-    bookModel
-      .updateBook(_id, req)
-      .then((data) => {
-        resolve(data);
-      })
-      .catch((err) => {
-        reject(err);
-      });
-  });
-};
-module.exports.deleteBook = (_id) => {
-  return new Promise((resolve, reject) => {
-    bookModel
-      .deleteBook(_id)
-      .then((data) => {
-        resolve(data);
-      })
-      .catch((err) => {
-        reject(err);
-      });
-  });
-};
+}
