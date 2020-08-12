acdc511296bd8e6bb05d385d750700cc2bef150b
vishnu vnaik <vishnuvdev17@gmail.com>
2020-08-12 21:05:54 +0530

    [Vishnu]Add.added some test cases for user functions and admin functions

--- test/bookServicestest.js
+++ test/bookServicestest.js
@@ -1,42 +1,42 @@
-
-const chai = require('chai');
-const chaiHttp = require('chai-http');
-const app = require('../server');
+const chai = require("chai");
+const chaiHttp = require("chai-http");
+const app = require("../server");
 
 chai.should();
 chai.use(chaiHttp);
-let fs = require('fs');
+let fs = require("fs");
 function readf() {
-
-    let data = fs.readFileSync(`${__dirname}/bookServices.json`);
-    let pData = JSON.parse(data);
-    return pData;
+  let data = fs.readFileSync(`${__dirname}/bookServices.json`);
+  let pData = JSON.parse(data);
+  return pData;
 }
 describe(`book list`, () => {
-    let data = readf();
-    it(`given a books  When  all books details are proper then return 200 status code`, (done) => {
-        chai.request(app)
-            .post('/books')
-            .send(data.addBook200)
-            .end((err, res) => {
-                if (err) {
-                    return done(err);
-                }
-                res.should.have.status(200);
-                done();
-            });
-    });
+  let data = readf();
+  it(`given a books  When  all books details are proper then return 200 status code`, (done) => {
+    chai
+      .request(app)
+      .post("/books")
+      .send(data.addBook200)
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+        res.should.have.status(200);
+        done();
+      });
+  });
 
-    it(`given a books  When  all books details are not proper then return 422 status code`, (done) => {
-        chai.request(app)
-            .post('/books')
-            .send(data.addBook422)
-            .end((err, res) => {
-                if (err) {
-                    return done(err);
-                }
-                res.should.have.status(200);
-                done();
-            });
-    });
+  it(`given a books  When  all books details are not proper then return 422 status code`, (done) => {
+    chai
+      .request(app)
+      .post("/books")
+      .send(data.addBook422)
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+        res.should.have.status(200);
+        done();
+      });
+  });
 });
